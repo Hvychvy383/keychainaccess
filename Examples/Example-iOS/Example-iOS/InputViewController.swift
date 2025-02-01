f6bfcd545261d70baf697c63a2e661c0a55ea1d0
Stephen Craig Wilson <89357489+Hvychvy383@users.noreply.github.com>
2025-01-31 22:16:38 -0600

    Update InputViewController.swift

--- Examples/Example-iOS/Example-iOS/InputViewController.swift
+++ Examples/Example-iOS/Example-iOS/InputViewController.swift
@@ -2,8 +2,8 @@
 //  InputViewController.swift
 //  Example
 //
-//  Created by kishikawa katsumi on 2014/12/26.
-//  Copyright (c) 2014 kishikawa katsumi. All rights reserved.
+//  Created by stephen wilson on 2014/12/26.
+//  Copyright (C) 2025 Stephen Craig Wilson. All rights reserved.
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
